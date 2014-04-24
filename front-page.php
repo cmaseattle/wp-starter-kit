1d18c7c008d99826b58f203db2e208d8ba6a8056
svmatthews <matthews.sam@gmail.com>
2014-04-24 14:00:43 -0700

    loop to front-page.php

--- front-page.php
+++ front-page.php
@@ -2,5 +2,24 @@
 
 <!-- homepage -->
 
+<!-- bare bones loop -->
+<?php
+$query = new WP_Query( 
+	array( 
+		/* your arguments */ 
+	)
+);
+if ( $query->have_posts() ) : ?>
+	<!-- the loop -->
+	<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+		<?php the_title(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; ?>
+	<!-- end of the loop -->
+	<?php wp_reset_postdata(); ?>
+<?php else:  ?>
+  <p><?php _e( 'Error message or content.' ); ?></p>
+<?php endif; ?>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
